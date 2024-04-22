a10789b5a1facd2e8076893b020ca5dad51fa1bc
jpb <jakub.baranski.33@gmail.com>
2024-04-22 22:27:18 +0100

    Add project files.

---
+++ ReboundHub/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using Microsoft.UI;
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using Microsoft.UI.Xaml.Navigation;
+global using ReboundHub.Common;
+global using ReboundHub.Views;
+global using WinUICommunity;
+global using static ReboundHub.Common.AppHelper;
+global using static ReboundHub.Common.LoggerSetup;
