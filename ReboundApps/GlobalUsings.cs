726b2f78437d172c984d4e347e4d588391f17abf
jpb <jakub.baranski.33@gmail.com>
2024-04-27 12:45:13 +0100

    Add ReboundApps

---
+++ ReboundApps/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using Microsoft.UI;
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using Microsoft.UI.Xaml.Navigation;
+global using ReboundApps.Common;
+global using ReboundApps.Views;
+global using WinUICommunity;
+global using static ReboundApps.Common.AppHelper;
+global using static ReboundApps.Common.LoggerSetup;
